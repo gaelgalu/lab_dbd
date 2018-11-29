bfff6f01ac2fa87083febccf4cda40a3d1bda9a6
Unknown <felipe.gonzalez.me@usach.cl>
2018-11-28 21:51:44 -0300

    Arreglo de PaymentMethod
    modelo y migración

--- database/migrations/2018_11_29_004917_create_payment_methods_table.php
+++ database/migrations/2018_11_29_004917_create_payment_methods_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePaymentMethodTable extends Migration
+class CreatePaymentMethodsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreatePaymentMethodTable extends Migration
      */
     public function up()
     {
-        Schema::create('PaymentMethod', function (Blueprint $table) {
+        Schema::create('payment_methods', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ class CreatePaymentMethodTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('PaymentMethod');
+        Schema::dropIfExists('payment_methods');
     }
 }
