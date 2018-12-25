83f03b0381f3527dadef1f6e46b823e2bb251f01
eliass1997ok <gabriel.gaete.l@usach.cl>
2018-12-25 19:37:51 -0300

    Using a package to convert from migrations to raw sql

--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        \Froiden\SqlGenerator\SqlGeneratorServiceProvider::class,
 
         /*
          * Package Service Providers...
