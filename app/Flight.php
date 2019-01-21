142ff7d0ddaef47528b3193bece297a370f886b4
eliass1997ok <gabriel.gaete.l@usach.cl>
2019-01-21 04:29:44 -0300

    Fixes on relationships.

--- app/Flight.php
+++ app/Flight.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Events\NewFlight;
 
 class Flight extends Model
 {
--- database/seeds/AirportFlightTableSeeder.php
+++ database/seeds/AirportFlightTableSeeder.php
@@ -16,15 +16,30 @@ class AirportFlightTableSeeder extends Seeder
     public function run()
     {
         $airport_id = Airport::inRandomOrder()->select('id')->get();
-        $flight_id = Flight::inRandomOrder()->select('id')->get();
+        $flight_id = Flight::inRandomOrder()->select('id', 'origin', 'destiny')->get();
 
-        for ($i=0; $i < count($airport_id); $i++){
-        	DB::table('airport_flight')->insert([
-        		'airport_id' => $airport_id[$i]->id,
-        		'flight_id' => $flight_id[$i]->id,
-        		'created_at' => Carbon::now(),
-        		'updated_at' => Carbon::now()
-        	]);
+        // for ($i=0; $i < count($airport_id); $i++){
+        	// DB::table('airport_flight')->insert([
+        	// 	'airport_id' => $airport_id[$i]->id,
+        	// 	'flight_id' => $flight_id[$i]->id,
+        	// 	'created_at' => Carbon::now(),
+        	// 	'updated_at' => Carbon::now()
+        	// ]);
+        // }
+
+        foreach ($flight_id as $flight) {
+            DB::table('airport_flight')->insert([
+                'airport_id' => $flight->origin,
+                'flight_id' => $flight->id,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+            DB::table('airport_flight')->insert([
+                'airport_id' => $flight->destiny,
+                'flight_id' => $flight->id,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
         }
     }
 }
